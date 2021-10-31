1d1a2cc0554c0bed82cf0c94e9015750e3624ff3
Rajat Gupta <rajatgupta@Rajats-MacBook-Air.local>
2021-11-01 00:10:28 +0530

    Initial Working

---
+++ src/com/rajat/learning/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.rajat.learning;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class WelcomeController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String showPage() {
+		return "main-menu";
+	}
+}
